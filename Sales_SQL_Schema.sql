10537b0271c3b3ba3e22a64fed9893cbe7b3da9b
Filiberto Villegas <filibertovillegas92@gmail.com>
2020-11-04 21:26:19 -0800

    added cleaned_stores csv and stores table to schema

--- Sales_SQL_Schema.sql
+++ Sales_SQL_Schema.sql
@@ -1,10 +1,20 @@
 create table Sales (
 	store varchar,
 	Dept varchar, 
-	sales_date date, 
+	sale_date date, 
 	weekly_sales float, 
 	IsHoliday boolean,
 	Primary Key ( sales_date, store)
 	)
 
 select * from sales
+
+create table stores(
+	store varchar, 
+	type varchar, 
+	size float
+
+)
+
+select * from stores
+
